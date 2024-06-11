6c63e17d6db9d01f37eeed8ca1171d2035b52d0d
Pardeep661 <maheyp666@gmail.com>
2024-06-11 13:25:00 +0100

    button declared

--- app/src/main/java/com/pardeep/calculator/MainActivity.kt
+++ app/src/main/java/com/pardeep/calculator/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.pardeep.calculator
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-//    var bt
+    var bt: Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
